8a361881a88506e764ecfc37a539662096d4bcbb
daahyunk <ekgusdl0408@naver.com>
2024-10-23 13:37:17 +0900

    ✨ Mission2 완료

--- week4/daahyunk/mission2/src/pages/movies/MoviesPage.jsx
+++ week4/daahyunk/mission2/src/pages/movies/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import MovieCard from '../components/movies/MovieCard';
+import MovieCard from '../../components/movies/MovieCard';
 import { MOVIES } from '../mocks/movies';
 
 const MovieListContainer = styled.div`
