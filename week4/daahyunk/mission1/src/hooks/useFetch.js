ca951aa2e10dbbc69e3759de228d409332e27712
daahyunk <ekgusdl0408@naver.com>
2024-10-23 12:26:01 +0900

    ✨ Mission1 완료

--- week4/daahyunk/mission1/src/hooks/useFetch.js
+++ week4/daahyunk/mission1/src/hooks/useFetch.js
@@ -6,7 +6,6 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // 환경 변수에서 토큰을 제대로 불러오는지 확인
     const token = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
     console.log('API Token:', token);
 
@@ -14,7 +13,7 @@ const useFetch = (url) => {
       try {
         const response = await fetch(url, {
           headers: {
-            Authorization: `Bearer ${token}`, // 환경 변수를 사용한 인증 헤더
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json;charset=utf-8',
           },
         });
