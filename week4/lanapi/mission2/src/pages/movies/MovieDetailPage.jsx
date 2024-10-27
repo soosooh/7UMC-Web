b0d991434e90551e5c67752bdc190483834d83db
lanapi <songhj1119@gmail.com>
2024-10-27 14:00:27 +0900

    4주차 최종본

--- week4/lanapi/mission2/src/pages/movies/MovieDetailPage.jsx
+++ week4/lanapi/mission2/src/pages/movies/MovieDetailPage.jsx
@@ -5,8 +5,7 @@ import axios from 'axios';
 import MovieDetailBanner from '../../components/movies/MovieDetailBanner';
 import styled from 'styled-components';
 
-const Token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTYzYzUwZGQwMjI5Y2ViMDUyZGM5ZTNlMGRlOWEyNSIsIm5iZiI6MTcyODI4ODY3NC4zNTAxNDEsInN1YiI6IjY2ZmViY2JhYzlhMTBkNDZlYTdjOWQ0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hiJR7nmiA0-Hbx5nJuALWjUN0IzIMpWtGzKXplLYSYw";
-
+const Token = "Bearer my Token";
 const MovieDetailPage = () => {
     const { movieId } = useParams(); // movieId 파라미터 가져오기
     const [movie, setMovie] = useState(null);
