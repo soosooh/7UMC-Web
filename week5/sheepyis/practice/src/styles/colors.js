4f39cdf00a622e729e03a500f1dbd1adb1cb31b6
sheepyis <113487989+sheepyis@users.noreply.github.com>
2024-10-28 17:52:24 +0900

    ✨week5_practice 완료

---
+++ week5/sheepyis/practice/src/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    navBackground: "#413F3F",
+    signBackground: "#FF073D",
+    error: "#FF0000",
+    main: "#FF073D",
+    black: "#000000",
+    white: "#FFFFFF",
+};
+
+export default colors;
