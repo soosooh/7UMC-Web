a0d293368b45df6a740341062c06cc85fcd377f2
chy031202 <chy031202@gmail.com>
2024-10-29 21:40:35 +0900

    ✨5주차 미션 완료

---
+++ week5/chy031202/mission/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import Loginuseform from "../components/login/loginUseForm";
+
+const Login = () => {
+    return(
+        <>
+        <Loginuseform/>
+        </>
+    );
+}
+
+export default Login;
