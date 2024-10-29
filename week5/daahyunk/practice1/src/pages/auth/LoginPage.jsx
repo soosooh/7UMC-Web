ee67d586c123e376e54b6ee23a14e4bc33e94bcc
daahyunk <ekgusdl0408@naver.com>
2024-10-30 02:16:41 +0900

    ✨ 유효성 검사 구현

--- week5/daahyunk/practice1/src/pages/auth/LoginPage.jsx
+++ week5/daahyunk/practice1/src/pages/auth/LoginPage.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useValidation } from "../../hooks/useValidation.js";
 
 const LoginPageContainer = styled.div`
   width: 100%;
@@ -9,7 +10,7 @@ const LoginPageContainer = styled.div`
   background-color: #000;
 `;
 
-const LoginForm = styled.form`
+const LoginForm = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -27,38 +28,75 @@ const Title = styled.h1`
 const Input = styled.input`
   width: 100%;
   padding: 1rem;
-  margin-bottom: 1rem;
   font-size: 0.9rem;
+  margin-top: 1rem;
   border: none;
   border-radius: 6px;
   outline: none;
 `;
 
+const ErrorMessage = styled.span`
+  width: 100%;
+  color: red;
+  font-size: 0.8rem;
+  margin-top: 0.8rem;
+  margin-bottom: 0.3rem;
+  text-align: left;
+`;
+
 const LoginButton = styled.button`
   width: 100%;
   padding: 1rem;
-  background-color: #ff4973;
+  background-color: ${(props) => (props.disabled ? "#555" : "#ff4973")};
   color: white;
   font-size: 1rem;
   font-weight: 500;
+  margin-top: 1rem;
   border: none;
   border-radius: 6px;
-  cursor: pointer;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
   transition: background-color 0.3s;
 
   &:hover {
-    background-color: #ff2a5f;
+    background-color: ${(props) => (props.disabled ? "#555" : "#ff2a5f")};
   }
 `;
 
 const LoginPage = () => {
+  const {
+    email,
+    password,
+    emailError,
+    passwordError,
+    isFormValid,
+    handleEmailChange,
+    handlePasswordChange,
+    handleBlur,
+  } = useValidation();
+
   return (
     <LoginPageContainer>
       <LoginForm>
         <Title>로그인</Title>
-        <Input type="email" placeholder="이메일을 입력해주세요!" />
-        <Input type="password" placeholder="비밀번호를 입력해주세요!" />
-        <LoginButton>로그인</LoginButton>
+        <Input
+          type="email"
+          placeholder="이메일을 입력해주세요!"
+          value={email}
+          onChange={handleEmailChange}
+          onBlur={() => handleBlur("email")}
+        />
+        {emailError && <ErrorMessage>{emailError}</ErrorMessage>}
+        
+        <Input
+          type="password"
+          placeholder="비밀번호를 입력해주세요!"
+          value={password}
+          onChange={handlePasswordChange}
+          onBlur={() => handleBlur("password")}
+        />
+        {passwordError && <ErrorMessage>{passwordError}</ErrorMessage>}
+        
+        <LoginButton disabled={!isFormValid}>로그인</LoginButton>
       </LoginForm>
     </LoginPageContainer>
   );
