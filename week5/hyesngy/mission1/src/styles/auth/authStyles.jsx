86b5390133bda850074b5e2f4f639d55be32961f
hyesngy <tell280210@gmail.com>
2024-10-31 11:21:31 +0900

    ✨ week5_mission1 완료

---
+++ week5/hyesngy/mission1/src/styles/auth/authStyles.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 25rem;
+  padding: 1rem;
+  border-radius: 8px;
+  margin-top: 1rem;
+`
+export const SubmitBtn = styled.button`
+  width: 25rem;
+  padding: 1rem;
+  border-radius: 8px;
+  background: ${(props) => (props.disabled ? 'gray' : '#F82F62')};
+  text-align: center;
+  color: white;
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
+  font-size: 1rem;
+  margin-top: 2rem;
+`
+export const ErrorMsg = styled.p`
+  color: #FF073D;
+  align-self: flex-start;
+`
