38486bdc8f00086bd1e6f9281f6451dc76937484
sheepyis <113487989+sheepyis@users.noreply.github.com>
2024-11-11 13:55:07 +0900

    🎉week7_mission2 project settings

---
+++ week7/sheepyis/mission2/src/components/movies/list-credit.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import ItemCredit from "./item-credit";
+
+const ListContainer = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    margin-top: 1vw;
+    gap: 1vw;
+`
+
+const ListCredit = ({ movie }) => {
+    console.log(movie);
+
+    const combinedCredits = [
+        ...movie.credits.cast.map(castMember => ({
+            name: castMember.name,
+            profile_path: castMember.profile_path,
+            known_for_department: castMember.known_for_department,
+        })),
+        ...movie.credits.crew.map(crewMember => ({
+            name: crewMember.name,
+            profile_path: crewMember.profile_path,
+            known_for_department: crewMember.known_for_department,
+        }))
+    ];
+
+    return (
+        <ListContainer>
+            {combinedCredits.map((credits, index) => (
+                <ItemCredit 
+                    key={index}
+                    name={credits.name}
+                    profile_path={`https://image.tmdb.org/t/p/w500${credits.profile_path}`}
+                    role={credits.known_for_department}
+                />
+            ))}
+        </ListContainer>
+    );
+}
+
+export default ListCredit;
