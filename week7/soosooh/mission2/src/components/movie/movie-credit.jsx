b58b60a199d313fa495866750490aec6547423b3
soohee Jang <shjang0820@hansung.ac.kr>
2024-11-13 13:56:50 +0900

    :sparkles: week7_mission2 완료

---
+++ week7/soosooh/mission2/src/components/movie/movie-credit.jsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import useCustomFetch from "../../hooks/useCunstomFetch";
+import CreditCard from "./credit-card";
+import SkeletonCredit from "../skeleton/skeleton-credit";
+
+const CreditTitle = styled.p`
+  font-size: 2.5em;
+  font-weight: bold;
+  margin-left: 50px;
+`;
+const CreditContainer = styled.div`
+  display: flex;
+  margin: 35px;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+const MovieCredit = ({ movie }) => {
+  const {
+    data: creditsData,
+    isLoading,
+    isError,
+  } = useCustomFetch(`/movie/${movie.id}/credits`);
+
+  // 데이터의 구조를 보장하기 위해 방어적인 코드 추가
+  const credits = creditsData?.pages?.[0] || {}; // 첫 번째 페이지만 사용하거나 빈 객체로 설정
+  const castList = credits.cast || []; // cast가 존재하지 않으면 빈 배열로 설정
+
+  if (isLoading) {
+    return <SkeletonCredit />;
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 중 입니다...</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      <CreditTitle>감독/출연</CreditTitle>
+      <CreditContainer>
+        {castList.map((castMember) => (
+          <CreditCard
+            key={castMember.id}
+            profile_path={castMember.profile_path}
+            original_name={castMember.original_name}
+            character={castMember.character}
+          />
+        ))}
+      </CreditContainer>
+    </>
+  );
+};
+
+export default MovieCredit;
---
+++ week7/soosooh/mission2/src/pages/movies/movies.jsx
@@ -0,0 +1,78 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import nowImg from "../../assets/images/image1.png";
+import popularImg from "../../assets/images/image2.png";
+import topImg from "../../assets/images/image3.png";
+import comingImg from "../../assets/images/image4.png";
+const MovieContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h1`
+  margin-left: 35px;
+  color: white;
+`;
+const CardContainer = styled.div`
+  display: flex;
+`;
+const ImageContainer = styled.div`
+  position: relative;
+  width: 277px;
+  height: 171px;
+  border-radius: 10px;
+  overflow: hidden;
+  margin-left: 35px;
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+  text {
+    background: ;
+  }
+`;
+const CardTitle = styled.span`
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+  padding: 4px;
+`;
+const Movies = () => {
+  return (
+    <MovieContainer>
+      <Title>카테고리</Title>
+      <CardContainer>
+        <ImageContainer>
+          <Link to="/movies/now-playing">
+            <img src={nowImg} alt="nowImg" />
+            <CardTitle>현재 상영중인</CardTitle>
+          </Link>
+        </ImageContainer>
+        <ImageContainer>
+          <Link to="/movies/popular">
+            <img src={popularImg} alt="popularImg" />
+            <CardTitle>인기있는</CardTitle>
+          </Link>
+        </ImageContainer>
+        <ImageContainer>
+          <Link to="/movies/top-rated">
+            <img src={topImg} alt="topImg" />
+            <CardTitle>높은 평가를 받은</CardTitle>
+          </Link>
+        </ImageContainer>
+        <ImageContainer>
+          <Link to="/movies/up-coming">
+            <img src={comingImg} alt="comingImg" />
+            <CardTitle>개봉 예정중인</CardTitle>
+          </Link>
+        </ImageContainer>
+      </CardContainer>
+    </MovieContainer>
+  );
+};
+
+export default Movies;
