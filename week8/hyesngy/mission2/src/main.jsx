0398e50f3985cb3471f0139de3f5ba9942e7fdb4
hyesngy <tell280210@gmail.com>
2024-11-21 00:01:20 +0900

    🔥 week8_mission2 ReactQueryDevtools 제거

--- week8/hyesngy/mission2/src/main.jsx
+++ week8/hyesngy/mission2/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import App from './App.jsx'
 
 const queryClient = new QueryClient();
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 )
