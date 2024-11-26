2cbfe02b10a54f672b1dec6b28dfa94a8d2dfc93
hyesngy <tell280210@gmail.com>
2024-11-26 16:53:03 +0900

    ✨ week9_practice 완료

--- week9/hyesngy/practice/src/App.jsx
+++ week9/hyesngy/practice/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+import InputTodo from "./components/InputTodo";
+import styled from "styled-components";
+import "./App.css";
+
+const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f4f4f9;
+  overflow: hidden;
+  box-sizing: border-box;
+`
+const TodoListContainer = styled.div`
+  margin-top: 1rem;
+  width: 100%;
+  max-width: 400px;
+  height: 45vh;
+  overflow-y: auto;
+  padding: 10px;
+
+  @media (max-width: 375px) {
+    height: 30vh;
+  }
+`
+const TodoContainer = styled.div`
+  background-color: white;
+  padding: 3rem 2.5rem;
+  border-radius: 2rem;
+  box-sizing: border-box;
+  height: 80vh;
+  
+  @media (max-width: 500px) {
+    width: 90vw;
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <PageContainer>
+      <TodoContainer>
+        <Header />
+        <InputTodo />
+        <TodoListContainer>
+          <TodoList />
+        </TodoListContainer>
+      </TodoContainer>
+    </PageContainer>
+  );
 }
 
-export default App
+export default App;
---
+++ week9/hyesngy/practice/src/components/TodoList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { remove, complete } from "../redux/todoSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+export default function TodoList() {
+	const todolist = useSelector((state) => state.todo);
+	const dispatch = useDispatch();
+
+	const trash = <FontAwesomeIcon icon={faTrashCan} />;
+
+	console.log(todolist);
+
+	const todolistView = todolist.map((todo, idx) => (
+		<li key={todolist[idx].id}>
+			<input
+
+				type="checkbox"
+				onChange={() => dispatch(complete(todolist[idx].id))}
+			/>
+			<div >
+				{todo.complete === false ? <>{todo.text}</> : <del>{todo.text}</del>}
+			</div>
+			<button
+
+				type="button"
+				onClick={() => dispatch(remove(todolist[idx].id))}
+			>
+				{trash}
+			</button>
+		</li>
+	));
+
+	return (
+		<>
+			<ul>{todolistView}</ul>
+		</>
+	);
+}
