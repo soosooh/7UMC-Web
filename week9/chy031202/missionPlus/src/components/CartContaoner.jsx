aaf1c6b0e413bd31834e27c9c87278485a0e6880
chy031202 <chy031202@gmail.com>
2024-11-26 22:40:23 +0900

    ✨초기화버튼 구현

--- week9/chy031202/missionPlus/src/components/CartContaoner.jsx
+++ week9/chy031202/missionPlus/src/components/CartContaoner.jsx
@@ -1,5 +1,6 @@
 
 import CartItem from "./CartItem"
+import NotCart from "./modals/notcart";
 
 import styled from "styled-components";
 import Footer from "./CartFooter";
@@ -11,19 +12,22 @@ const CartContainer = () =>{
     //const cartItems = useStore((state) =>state.cartItems);
     const cartItems = useCartStore((state) => state.cartItems); // Zustand 상태 가져오기
     const clearCart = useCartStore((state) => state.clearCart); // Zustand 메서드 예시
+    const amount = useCartStore((state)=> state.amount);
 
-    
-    
     return(
         <Wrapp>
         <Header>
             <h2>당신이 선택한 음반</h2>
         </Header>
-        <Contents style={{width:'65vw',}}>
-            {cartItems.map((item) =>{
-                return <CartItem key={item.id} {...item} />
-            })}
-        </Contents>
+            {amount > 0 ? (
+                    <Contents style={{ width: "65vw" }}>
+                        {cartItems.map((item) => (
+                            <CartItem key={item.id} {...item} />
+                        ))}
+                    </Contents>
+                ) : (
+                    <NotCart /> // amount가 0일 때 NotCart 컴포넌트 렌더링
+                )}
         <footer>
             <Footer />
         </footer>
---
+++ week9/chy031202/missionPlus/src/components/modals/notcart.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const NotCart = () =>{
+    return(
+        <Wrapp>
+            <h5>고객님이 좋아하는 음반을 담아보세요~!</h5>
+        </Wrapp>
+    )
+}
+
+const Wrapp = styled.main`
+width:100%;
+height:100vh;
+color:#6D6FFF;
+`
+
+export default NotCart;
