b5ce42d243594cbb36fc8279f854630ba7ab344a
양인서 <113487989+sheepyis@users.noreply.github.com>
2024-12-07 22:23:55 +0900

    ✨week10_mission1 google maps 완료

--- week10/sheepyis/mission1/src/App.jsx
+++ week10/sheepyis/mission1/src/App.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layout/root-layout";
 import GlobalStyle from "./styles/globalStyles";
 import HomePage from "./pages/home/home";
@@ -10,83 +10,90 @@ import NowPlayingPage from "./pages/movies/nowPlaying";
 import PopularPage from "./pages/movies/popular";
 import TopRatedPage from "./pages/movies/topRated";
 import UpComingPage from "./pages/movies/upComing";
+import MapsPage from "./pages/maps/maps";
 import MovieDetail from "./pages/movies/movieDetail";
 import NotFoundPage from "./pages/notFound/notFound";
+import { LoadScript } from "@react-google-maps/api";
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <RootLayout />,
-      errorElement: <NotFoundPage />,
-      
-      children: [
-        {
-          index: true,
-          element: <HomePage />
-        },
-        {
-          path: 'login',
-          errorElement: <NotFoundPage />,
-          children: [
-            {
-              index: true,
-              element: <LoginPage />,
-            },
-            {
-              path: 'auth',
-              element: <LoginPage />
-            },
-          ]
-        },
-        {
-          path: 'signup',
-          element: <SignUpPage />
-        },
-        {
-          path: 'search',
-          element: <SearchPage />
-        },
-        {
-          path: 'movies',
-          errorElement: <NotFoundPage />,
-          children: [
-            {
-              index: true,
-              element: <CategoryPage />,
-            },
-            {
-              path: 'now-playing',
-              element: <NowPlayingPage />
-            },
-            {
-              path: 'popular',
-              element: <PopularPage />
-            },
-            {
-              path: 'top-rated',
-              element: <TopRatedPage />
-            },
-            {
-              path: 'up-coming',
-              element: <UpComingPage />
-            },
-            {
-              path:':movieId',
-              element: <MovieDetail />
-            }
-          ]
-        }
-      ]
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "login",
+        errorElement: <NotFoundPage />,
+        children: [
+          {
+            index: true,
+            element: <LoginPage />,
+          },
+          {
+            path: "auth",
+            element: <LoginPage />,
+          },
+        ],
+      },
+      {
+        path: "signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+      {
+        path: "map",
+        element: <MapsPage />,
+      },
+      {
+        path: "movies",
+        errorElement: <NotFoundPage />,
+        children: [
+          {
+            index: true,
+            element: <CategoryPage />,
+          },
+          {
+            path: "now-playing",
+            element: <NowPlayingPage />,
+          },
+          {
+            path: "popular",
+            element: <PopularPage />,
+          },
+          {
+            path: "top-rated",
+            element: <TopRatedPage />,
+          },
+          {
+            path: "up-coming",
+            element: <UpComingPage />,
+          },
+          {
+            path: ":movieId",
+            element: <MovieDetail />,
+          },
+        ],
+      },
+    ],
   },
-])
+]);
 
 function App() {
-    return (
-      <>
-        <GlobalStyle />
-        <RouterProvider router={router}/>
-      </>
-    )
+  return (
+    <>
+      <GlobalStyle />
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_TOKEN} libraries={["places"]}>
+        <RouterProvider router={router} />
+      </LoadScript>
+    </>
+  );
 }
 
 export default App;
