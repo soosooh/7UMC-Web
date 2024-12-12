67bb36cb9ae6cc526e4687ede36e55f8a173203a
soohee Jang <shjang0820@hansung.ac.kr>
2024-12-13 01:59:46 +0900

    🎉  week10_mission1 build setting

---
+++ week10/soosooh/mission1/src/components/movie/movie-list.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../../apis/axios-instance";
+import MovieCard from "./movie-card";
+import SkeletonCard from "../skeleton/skeleton-card";
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 30px;
+  justify-content: flex-start;
+  gap: 20px;
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  width: 100%;
+`;
+
+const PageButton = styled.button`
+  background-color: ${({ disabled }) => (disabled ? "#888" : "#ff073d")};
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin: 0 5px;
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  border-radius: 5px;
+
+  &:hover {
+    background-color: ${({ disabled }) => (disabled ? "#888" : "#ff073d")};
+  }
+`;
+
+const PageInfo = styled.span`
+  color: white;
+  margin: 0 10px;
+`;
+
+const MovieList = ({ category }) => {
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["movies", category, page],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/movie/${category}`, {
+        params: { page, language: "ko-KR" },
+      });
+      return response.data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <Container>
+        {Array.from({ length: 20 }).map((_, index) => (
+          <SkeletonCard key={index} />
+        ))}
+      </Container>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 중 입니다...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <ListContainer>
+      <Container>
+        {data?.results.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            release_date={movie.release_date}
+            poster_path={movie.poster_path}
+            overview={movie.overview}
+          />
+        ))}
+      </Container>
+      <PaginationContainer>
+        <PageButton
+          disabled={page === 1}
+          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+        >
+          이전
+        </PageButton>
+        <PageInfo>{`${page} 페이지`}</PageInfo> {/* 페이지 정보 표시 */}
+        <PageButton onClick={() => setPage((prev) => prev + 1)}>
+          다음
+        </PageButton>
+      </PaginationContainer>
+    </ListContainer>
+  );
+};
+
+export default MovieList;
---
+++ week10/soosooh/mission1/src/pages/login.jsx
@@ -0,0 +1,172 @@
+import styled from "styled-components";
+import useForm from "../hooks/use-form";
+import { validateLogin } from "../utils/validate";
+import { axiosAuth } from "../apis/axios-auth";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+
+const LogInContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 35%;
+  left: 40%;
+
+  @media screen and (max-width: 768px) {
+    position: static;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 20px;
+  }
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: 450px;
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const StyledInput = styled.input`
+  width: 450px;
+  height: 50px;
+  box-sizing: border-box;
+  padding: 0;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: gray;
+  border-radius: 8px;
+  background-color: white;
+  color: black;
+
+  @media (max-width: 1200px) {
+    width: 400px;
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const StyledSubmit = styled.button`
+  width: 450px;
+  height: 50px;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  background-color: #ff073d;
+  border: #ff073d;
+  border-radius: 8px;
+
+  @media (max-width: 1200px) {
+    width: 400px;
+    margin-right: 50px;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    margin-left: 50px;
+  }
+`;
+const ErrorText = styled.p`
+  color: red;
+  font-size: 12px;
+  position: absolute;
+  top: 55px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 10px;
+  }
+`;
+
+const LogInPage = () => {
+  const navigate = useNavigate();
+  const login = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: validateLogin,
+  });
+
+  const handlePressLogin = async () => {
+    const newErrors = login.validate(login.values);
+    login.setErrors(newErrors);
+
+    // 오류가 없을 경우에만 요청 진행
+    if (!Object.values(newErrors).some((error) => error)) {
+      mutation.mutate({
+        email: login.values.email,
+        password: login.values.password,
+      });
+    }
+  };
+
+  const mutation = useMutation({
+    mutationFn: async (userData) => {
+      return await axiosAuth.post("/auth/login", userData);
+    },
+    onSuccess: (response) => {
+      const { accessToken, refreshToken } = response.data;
+
+      // 토큰을 로컬 저장소에 저장
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      const userName = login.values.email.split("@")[0];
+      localStorage.setItem("userName", userName);
+      alert("로그인 성공!");
+      console.log("로그인 성공:", response.data);
+      navigate("/");
+      window.location.reload();
+    },
+    onError: (error) => {
+      console.error(
+        "로그인 실패:",
+        error.response ? error.response.data : error.message
+      );
+      alert(
+        `로그인 실패: ${
+          error.response?.data?.message || "서버 오류가 발생했습니다."
+        }`
+      );
+    },
+  });
+
+  return (
+    <LogInContainer>
+      <InputWrapper>
+        <StyledInput
+          error={login.touched.email && login.errors.email}
+          type={"email"}
+          placeholder={"이메일을 입력해주세요! "}
+          {...login.getTextInputProps("email")}
+        />
+        {login.touched.email && login.errors.email && (
+          <ErrorText>{login.errors.email}</ErrorText>
+        )}
+      </InputWrapper>
+      <InputWrapper>
+        <StyledInput
+          error={login.touched.password && login.errors.password}
+          type={"password"}
+          placeholder={"비밀번호를 입력해주세요! "}
+          {...login.getTextInputProps("password")}
+        />
+        {login.touched.password && login.errors.password && (
+          <ErrorText>{login.errors.password}</ErrorText>
+        )}
+      </InputWrapper>
+      <StyledSubmit onClick={handlePressLogin} disabled={mutation.isLoading}>
+        로그인
+      </StyledSubmit>
+    </LogInContainer>
+  );
+};
+
+export default LogInPage;
