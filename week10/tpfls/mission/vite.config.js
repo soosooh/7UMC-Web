b185fab814a5f5a47d0f352393d5681f95d84714
tpfls <165572388+tpfls@users.noreply.github.com>
2024-12-15 19:18:21 +0900

    mission

---
+++ week10/tpfls/mission/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // 빌드 결과물을 저장할 폴더
+  },
+});
